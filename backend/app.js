8bcf7ac120d87053fbea704556e7c64b5699bc0b
MayankMx13 <mayankmishra3214@gmail.com>
2024-11-25 05:16:57 +0530

    port

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const DATABASE = process.env.DATABASE_URI;
 const PORT = process.env.PORT || 4000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "bloggers-bcl6ftaj4-mayank-mishras-projects-095edd18.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
