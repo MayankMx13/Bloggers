7c5f81ea64aa2698531fcea29c5529afe3ad12d0
MayankMx13 <mayankmishra3214@gmail.com>
2024-11-25 21:52:29 +0530

    commit

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -75,7 +75,7 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       maxAge: age,
     });
