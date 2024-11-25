406a8c78c531a967873096f1bd01de901087af6b
MayankMx13 <mayankmishra3214@gmail.com>
2024-11-25 21:34:39 +0530

    commit

--- client/src/components/blogall.jsx
+++ client/src/components/blogall.jsx
@@ -18,7 +18,8 @@ function Blogall() {
     }, []);
 
     return (<>
-        <h2 className="font-bold justify-center items-center text-center flex text-xl">All Posts</h2>
+        <h2 className="font-bold justify-center items-center text-center flex text-xl
+        mt-5">All Posts</h2>
         <div className="bottom  md:mt-xl py-12 flex gap-2 sm:gap-3 md:gap-5 lg:gap-4 flex-wrap items-center justify-center">
 
 
